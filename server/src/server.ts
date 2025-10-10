4609df5544c223975997407f46f3f9ff6737fc1c
jou-coding <Jou.Moriyama05@gmail.com>
2025-10-10 16:51:52 +0900

    feat:新規登録のapi追加

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,10 @@
 import Express from "express";
 import http from "http"
+import bcrypt from "bcrypt"
+import { PrismaClient } from '../generated/prisma/client.js'
+
+// プリズマクライアントの作成
+const prisma = new PrismaClient()
 
 // Expressアプリの作成
 const app = Express();
@@ -11,6 +16,39 @@ app.use(Express.static("public"))
 //Jsonリクエストをパースしていない
 app.use(Express.json())
 
+
+// 新規登録　POST /api/auth/register - 新規登録
+app.post("/api/auth/register",async (req,res) =>{
+  try{
+  const {username ,password}  = req.body
+
+   // バリデーション
+    if (!username || !password) {
+      return res.status(400).json({ 
+        status: false, 
+        error: "ユーザー名とパスワードは必須です" 
+      });
+    }
+  // パスワードをハッシュ化
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+   await prisma.user.create({
+    data: {
+      name: username,
+      password: hashedPassword,
+    },
+  });
+
+  res.send({status:true})
+  } catch(error){
+    console.error("Registration error:", error);
+
+     res.status(500).json({ 
+      status: false, 
+      error: "サーバーエラーが発生しました" 
+    });
+  }
+})
 
 server.listen(3000, () => {
   console.log("http://localhost:3000");
