48eefb47f571eaf48e7a06a7ebe3a7d89dbe92f4
jou-coding <Jou.Moriyama05@gmail.com>
2025-10-17 12:06:58 +0900

    feat:ログインへの遷移機能を追加

--- client/src/pages/FirstScreen.tsx
+++ client/src/pages/FirstScreen.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import {Link} from "react-router-dom"
 
 const FirstScreen:React.FC =() => {
     return(
@@ -6,7 +7,7 @@ const FirstScreen:React.FC =() => {
         <div className="flex min-h-screen items-center justify-center bg-gray-50 px-4 py-8">
             <div className="w-full max-w-md transform translate-y-6 sm:translate-y-8 flex gap-5 items-center justify-center">
                 <div className=" px-6 py-2 bg-blue-300 rounded-lg hover:bg-blue-400 transitoin duration-300 text-center">新規</div>
-                <div className="px-6 py-2 bg-blue-300 rounded-lg  hover:bg-blue-400 transition duration-300 text-center">ログイン</div>
+                <Link to="/resisteruser" className="px-6 py-2 bg-blue-300 rounded-lg  hover:bg-blue-400 transition duration-300 text-center" >ログイン</Link>
             </div>
         </div>
         
