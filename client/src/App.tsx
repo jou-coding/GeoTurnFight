5001f9df62c9e08cc3eef6c7dfacaa9d5cdc1c0b
jou-coding <Jou.Moriyama05@gmail.com>
2025-10-17 11:31:19 +0900

    feat:ResisterUserページを追加

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import FirstScreen from "./pages/FirstScreen";
+import ResisterUser from "./pages/ResisterUser";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<FirstScreen />} />
+        <Route path="/resisteruser" element={<ResisterUser />} />
       </Routes>
     </BrowserRouter>
   );
