cfa44da918820060f8529234d1928ccae243696e
jou-coding <Jou.Moriyama05@gmail.com>
2025-10-22 13:50:17 +0900

    fix: プレースホルダーを国名を入力に変更

--- client/src/pages/SendCountry.tsx
+++ client/src/pages/SendCountry.tsx
@@ -6,7 +6,7 @@ const  SendCountry:React.FC = () => {
             <div className=" flex items-center justify-center">
                 <div className="bg-white shadow-lg rounded-2xl p-6 sm:p-8 space-y-4">
                     <h2 className="text-2xl font-bold text-center">国名入力</h2>
-                    <input type="text"  placeholder="部屋のID入力" className="w-full p-2 border rounded-md focus:ring-2 focus:ring-blue-400"/>
+                    <input type="text"  placeholder="国名を入力" className="w-full p-2 border rounded-md focus:ring-2 focus:ring-blue-400"/>
                     <Button name="/battle" buttonName="決定"></Button>
                     <div className="border">
                         <h2 className="font-bold text-center">履歴</h2>
