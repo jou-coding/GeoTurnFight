c8ccf9142130eebeeb96b6492e0b291853b47e4e
jou-coding <Jou.Moriyama05@gmail.com>
2025-11-27 18:14:05 +0900

    feat: nameの変更の追加

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -1,11 +1,17 @@
-const Card = () => {
+import React from "react"
+
+type CardProps = {
+    title?:string
+}
+
+const Card:React.FC<CardProps> = ({title}) => {
     return(
         <div className= "bg-white rounded-lg shadow-md overflow-hidden hover:shadow-xl transition duration-300 flex " >
              <div className="w-5/12 flex justify-center">
                 <img src="..." className="rounded-full bg-blue-400 w-48 h-48 object-cover" />
              </div>
              <div className="w-7/12 pl-6 flex  items-center justify-start">
-                <h3 className="text-xl font-bold mb-2">タイトル</h3>
+                {title ? (<h3 className="text-xl font-bold mb-2">{title}</h3>):(<h3 className="text-xl font-bold mb-2">タイトル</h3>)  }
              </div>
         </div>
     )
--- client/src/pages/Battle.tsx
+++ client/src/pages/Battle.tsx
@@ -1,12 +1,20 @@
 import Button from "../components/Button"
 import Card from "../components/Card"
 
+const hoge = {
+    title:"hoge"
+}
+
+const hoi = {
+    title:"hoge"
+}
+
 const Battle = () => {
     return(
         <div className="min-h-screen bg-gray-50 flex justify-center  items-center flex-col">
             <div className="w-full max-w-md space-y-4">
-                <Card />
-                <Card />
+                <Card title={hoge.title}/>
+                <Card title={hoi.title} />
             </div>
             <Button name="/sendcountry" buttonName="決定"/>
         </div>
