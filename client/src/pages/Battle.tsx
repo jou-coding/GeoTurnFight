6eb23b8f7df34017e9f489ccd4023e5229ca3ea2
jou-coding <Jou.Moriyama05@gmail.com>
2025-11-27 19:07:08 +0900

    feat: 準備OK化を聞くボタンを追加

--- client/src/pages/Battle.tsx
+++ client/src/pages/Battle.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 import Button from "../components/Button"
 import Card from "../components/Card"
 
@@ -10,13 +11,28 @@ const hoi = {
 }
 
 const Battle = () => {
+
+    const [card,setCard] = useState(false)
+
+    useEffect(()=>{
+         let btn = document.getElementById("btn")
+    btn?.addEventListener("click",() => {
+        setCard(true)
+    })
+
+    },[])
+
+   
+    
+
     return(
         <div className="min-h-screen bg-gray-50 flex justify-center  items-center flex-col">
             <div className="w-full max-w-md space-y-4">
                 <Card title={hoge.title}/>
                 <Card title={hoi.title} />
             </div>
-            <Button name="/sendcountry" buttonName="決定"/>
+            {card ?<Button name="/sendcountry" buttonName="決定"/> :<div id="btn" className="bg-white rounded-lg  p-3 hover:shadow-lg">準備OK</div>}
+            
         </div>
 
     )
