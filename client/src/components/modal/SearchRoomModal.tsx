1991a33ddfdc84b8461840876a3d21a610bf0e93
jou-coding <Jou.Moriyama05@gmail.com>
2025-12-01 21:12:06 +0900

    fix: SerchRoomMoalをmodalに入れる

--- client/src/components/modal/SearchRoomModal.tsx
+++ client/src/components/modal/SearchRoomModal.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import Button from "./Button"
+import Button from "../Button"
 
 type Props = {
     onClose:() =>  void
--- client/src/pages/Room/Room.tsx
+++ client/src/pages/Room/Room.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { SearchRoomModal } from "../../components/SearchRoomModal";
+import { SearchRoomModal } from "../../components/modal/SearchRoomModal";
 
 const Room:React.FC = () => {
 
