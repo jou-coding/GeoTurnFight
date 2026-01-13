5f9ba45c0922b7491c8b9a1ef782ae7279804ebf
jou-coding <Jou.Moriyama05@gmail.com>
2026-01-13 10:09:41 +0900

    feat: findRoomApiにjwtを追加

--- client/src/components/modal/SearchRoomModal.tsx
+++ client/src/components/modal/SearchRoomModal.tsx
@@ -8,6 +8,10 @@ type Props = {
 export const SearchRoomModal:React.FC<Props> = ({onClose}) => {
     const [roomName,setRoomName] = useState("")
 
+
+    // token
+    const token = localStorage.getItem("accessToken")
+
     const  changeRoomName = (e:React.ChangeEvent<HTMLInputElement>) => {
         setRoomName(e.target.value)
     }
@@ -15,7 +19,7 @@ export const SearchRoomModal:React.FC<Props> = ({onClose}) => {
     const SearchRoom = async () => {
         const res = await fetch("http://localhost:3000/api/room/findRoom",{
         method:"POST",
-        headers:{"Content-Type":"application/json"},
+        headers:{"Content-Type":"application/json","Authorization": `Bearer ${token}`},
         body:JSON.stringify({name:roomName})
     }
 )
