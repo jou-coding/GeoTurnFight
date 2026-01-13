4325a6b4613bf39bd04f51349e52bbd4dc09f60a
jou-coding <Jou.Moriyama05@gmail.com>
2026-01-13 09:51:39 +0900

    remove: swaggerUIのコードの削除

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,6 @@
 import Express from "express";
 import http from "http"
 import dotenv from "dotenv";
-import swaggerUi from "swagger-ui-express";
-import fs from "fs";
-import path from "path";
-import YAML from "yaml";
 import cors from "cors"
 import { authRouter } from "./features/auth/routes.js";
 import { roomRouter } from "./features/room/routes.js";
@@ -14,13 +10,6 @@ import { authMiddleware } from "./middlewares/authMiddleware.js";
 dotenv.config()
 // Expressアプリの作成
 const app = Express();
-// openapi.yml の読み込み
-const openapiPath = path.join(process.cwd(), "src/openapi/openapi.yml");
-const file = fs.readFileSync(openapiPath, "utf8");
-const openapiDoc = YAML.parse(file);
-// Swagger UI
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(openapiDoc));
-
 // HTTPサーバーを作成し、Expressを使う
 const server = http.createServer(app)
 // 静的ファイル（HTML/CSS/JSなど)を配信
